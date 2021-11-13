247c99cdf24524498b4187fa395311d2e9aad069
MrOctopus <shock.game@hotmail.com>
2021-11-13 05:45:35 +0100

    Added collection of general logs, process logs, and general logs

--- pyWhatsUpp/collector/collect_images.py
+++ pyWhatsUpp/collector/collect_images.py
@@ -4,38 +4,51 @@ import re
 
 _JPEG_RE = re.compile("ffd8ff(.*?)ffd9")
 
-def _carve_file_for_jpeg(file_path, found_images):
-    if not os.path.isfile(file_path):
-        return None
+def _carve_file_for_jpeg(file_path):
+    images = []
 
     with open(file_path, 'rb') as file:
         hexdump = binascii.hexlify(file.read())
 
         for match in _JPEG_RE.finditer(hexdump.decode()):
             image_binary = binascii.a2b_hex(match[0])
-            found_images.append(image_binary)
+            images.append(image_binary)
 
-def run(info):
-    found_images = []
+    return images
 
-    for _dir in os.listdir(info.input):
-        # See https://blog.group-ib.com/whatsapp_forensic_artifacts
-        avatar_cache = os.path.join(info.input, _dir, "Cache", "data_2")
-        _carve_file_for_jpeg(avatar_cache, found_images)
-
-    # Could not find any images
-    if len(found_images) < 1:
+def _collect_images(info, avatar_caches):
+    if len(avatar_caches) < 1:
         return False
 
     images_dir = os.path.join(info.output, "Contact Avatars")
     os.mkdir(images_dir)
+    num_images = 0
 
-    info.log.info(f"Created new contact avatars folder containing a total of '{len(found_images)}' images")
+    for avatar_cache in avatar_caches:
+        images = _carve_file_for_jpeg(avatar_cache)
+        
+        for image in images:
+            image_dest = os.path.join(images_dir, f"{num_images}.jpg")
 
-    for i, image in enumerate(found_images):
-        image_path = os.path.join(images_dir, f"{i}.jpg")
+            with open(image_dest,'wb') as file:
+                file.write(image)
 
-        with open(image_path,'wb') as file:
-            file.write(image)
+            num_images += 1
 
+    if num_images < 1:
+        return False
+
+    info.log.info(f"Created new contact avatars folder containing a total of '{num_images}' images")
     return True
+
+def run(info):
+    avatar_caches = []
+
+    for _dir in os.listdir(info.input):
+        # See https://blog.group-ib.com/whatsapp_forensic_artifacts
+        cache_file = os.path.join(info.input, _dir, "Cache", "data_2")
+        
+        if os.path.isfile(cache_file):
+            avatar_caches.append(cache_file)
+
+    return _collect_images(info, avatar_caches)
--- pyWhatsUpp/collector/collect_logs.py
+++ pyWhatsUpp/collector/collect_logs.py
@@ -1,2 +1,105 @@
+import os
+import glob
+import shutil
+import binascii
+import re
+
+_EVENT_RE = re.compile("6c6f6722(.*?)74696d657374616d70")
+
+def _collect_general_logs(info, logs):
+    if len(logs) < 1:
+        return False
+
+    general_logs_dir = os.path.join(info.output, "General Logs")
+    os.mkdir(general_logs_dir)
+
+    for i, log in enumerate(logs):
+        log_dest = os.path.join(general_logs_dir, f"{i}_{os.path.basename(log)}")
+        shutil.copy2(log, log_dest)
+
+    return True
+
+def _collect_process_logs(info, logs):
+    if len(logs) < 1:
+        return False
+
+    process_logs_dir = os.path.join(info.output, "Process Logs")
+    os.mkdir(process_logs_dir)
+
+    for i, log in enumerate(logs):
+        log_dest = os.path.join(process_logs_dir, f"{i}_{os.path.basename(log)}")
+        shutil.copy2(log, log_dest)
+    
+    return True
+
+def _carve_file_for_events(file_path):
+    events = []
+
+    with open(file_path, 'rb') as file:
+        hexdump = binascii.hexlify(file.read())
+
+        for match in _EVENT_RE.finditer(hexdump.decode()):
+            event = str(binascii.a2b_hex(match.group(1)))
+            # Since we cannot decode the string directly, we need to pretty up the string with hacks
+            # START: Find first space to remove gibberish
+            # END: -4 removes trailing "\t'
+            event = event[event.find(' ') + 1:-4]
+
+            events.append(event)
+
+    return events
+
+def _collect_event_logs(info, logs):
+    if len(logs) < 1:
+        return False
+
+    event_logs_dir = os.path.join(info.output, "Event Logs")
+    os.mkdir(event_logs_dir)
+    num_logs = 0
+
+    for log in logs:
+        events = _carve_file_for_events(log)
+
+        if len(events) < 1:
+            continue
+
+        log_dest = os.path.join(event_logs_dir, f"{num_logs}_{os.path.basename(log)}")
+
+        with open(log_dest, 'w+') as file:
+            file.write('\n'.join(events))
+        
+        shutil.copystat(log, log_dest)
+        num_logs += 1
+
+    if num_logs < 1:
+        return False
+
+    return True
+
 def run(info):
-    return False
+    input_glob = os.path.join(info.input, '**', '*[Ll][Oo][Gg]*')
+    glob_matches = glob.glob(input_glob, recursive=True)
+
+    # Could not find any log files
+    if not glob_matches:
+        return False
+
+    general_logs = []
+    process_logs = []
+    event_logs = []
+
+    for log in glob_matches: 
+        if "process" in log:
+            process_logs.append(log)
+        elif "index" in log:
+            event_logs.append(log)
+        else:
+            general_logs.append(log)
+
+    successful = 0
+    
+    successful += int(_collect_general_logs(info, general_logs))
+    successful += int(_collect_process_logs(info, process_logs))
+    successful += int(_collect_event_logs(info, event_logs))
+
+    return successful > 0
